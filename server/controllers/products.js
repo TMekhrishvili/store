7d48b9a7cdcfb738b75c487fdcc4d17d080e7d1f
Tamar Mekhrishvili <tmekh11@freeuni.edu.ge>
2022-01-15 00:40:18 +0400

    feat: search

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -1,7 +1,22 @@
 const Product = require('../models/Products');
 
 const getAllProducts = async (req, res) => {
-    const data = await Product.find();
+    // search params
+    const { name, featured, company, sort } = req.query;
+    const params = {};
+    if (name) params.name = { $regex: name, $options: 'i' };
+    if (featured) params.featured = featured;
+    if (company) params.company = company;
+    // get products
+    let products = Product.find(params);
+    // sort
+    if (sort) {
+        const list = sort.split(',').join(' ');
+        products = products.sort(list);
+    } else {
+        products = products.sort('createdAt');
+    }
+    const data = await products;
     res.status(200).json({ success: true, errors: null, data });
 }
 
