62626e7c23eefc987fbd76c7b36eeeb5498746a3
Tamar Mekhrishvili <tmekh11@freeuni.edu.ge>
2022-01-15 12:33:33 +0400

    feat: pagination

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -16,6 +16,13 @@ const getAllProducts = async (req, res) => {
     } else {
         products = products.sort('createdAt');
     }
+
+    // pagination
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+    products = products.skip(skip).limit(limit);
+
     const data = await products;
     res.status(200).json({ success: true, errors: null, data });
 }
